013b959d416107f086cbe71519c4034f5709372b
luis.ventura@tecsup.edu.pe <102069450+Angellvz@users.noreply.github.com>
2022-10-10 21:20:25 -0500

    javascript-poo

---
+++ POO/POOJ-JavaScript/programacion orientada a objetos-1.js
@@ -0,0 +1,14 @@
+
+//Definicion y usos
+class animal{
+    constructor(especie, edad, color){
+        this.especie=especie;
+        this.edad=edad;
+        this.color=color;
+
+    }
+}
+const perro= new animal()
+document.write(perro)
+
+//mas info == https://bluuweb.github.io/desarrollo-web-bluuweb/11-11-js-poo/#ejemplo-teorico
